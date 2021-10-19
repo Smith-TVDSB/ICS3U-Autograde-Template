2261aa73dfb44c356cadd794ecdfe316b87adf75
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2021-10-19 15:05:35 -0400

    Initial commit

---
+++ student.hpp
@@ -0,0 +1,16 @@
+#pragma once // prevents multiple definitions
+
+#include <iostream> // print library
+#include <string>
+using namespace std;
+
+// ===============
+// implementations
+
+//Replace string function
+string replace(string sentence, string word, string replacingWord)
+{
+	return "0"; // TODO: fix
+}
+
+
